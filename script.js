f689165eec7f16d38fae0b5fdfa9ddd635a86aa1
4d616c616b <124023463+yussypu@users.noreply.github.com>
2024-02-26 10:09:55 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
 function openLetter() {
-  document.getElementById("truck-container").style.left = "100%";
-  setTimeout(() => {
-    document.getElementById("letter-container").style.display = "block";
-    document.getElementById("letter-content").innerHTML = "Thank you for the drawing Nehir, graphic design really is your passion <333";
-  }, 2000); // Wait for the truck to reach the end
+  document.getElementById("letter-container").style.display = "block";
+  document.getElementById("letter-content").innerHTML = "Thank you for the drawing Nehir, graphic design really is your passion <333";
 }
