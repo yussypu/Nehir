cade3a5b5dda10600fda535a940df9f57cd5a91d
4d616c616b <124023463+yussypu@users.noreply.github.com>
2024-02-26 10:14:20 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,26 @@
-function openLetter() {
-  document.getElementById("letter-container").style.display = "block";
-  document.getElementById("letter-content").innerHTML = "Thank you for the drawing Nehir, graphic design really is your passion <333";
-}
+const wrapper = document.querySelector(".wrapper");
+const question = document.querySelector(".question");
+const gif = document.querySelector(".gif");
+const yesBtn = document.querySelector(".yes-btn");
+const noBtn = document.querySelector(".no-btn");
+
+yesBtn.addEventListener("click", () => {
+  // Update the response for the "Yes" button
+  question.innerHTML = "Thank you for the drawing Nehir, graphic design really is your passion <333";
+  gif.src = "https://media.giphy.com/media/UMon0fuimoAN9ueUNP/giphy.gif";
+  // Remove buttons
+  yesBtn.style.display = "none";
+  noBtn.style.display = "none";
+});
+
+noBtn.addEventListener("mouseover", () => {
+  const noBtnRect = noBtn.getBoundingClientRect();
+  const maxX = window.innerWidth - noBtnRect.width;
+  const maxY = window.innerHeight - noBtnRect.height;
+
+  const randomX = Math.floor(Math.random() * maxX);
+  const randomY = Math.floor(Math.random() * maxY);
+
+  noBtn.style.left = randomX + "px";
+  noBtn.style.top = randomY + "px";
+});
